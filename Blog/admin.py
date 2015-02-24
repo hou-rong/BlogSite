9093852ad9bd1c6ce7af6b3d122bf47f5b6fa1ce
hourong <nmghr9@gmail.com>
2015-02-24 23:43:38 +0800

    Create Tags and Category.

--- Blog/admin.py
+++ Blog/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from .models import *
 
 
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'createTime', 'updateTime']
+# class BlogAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'createTime', 'updateTime']
+#
+#     class Meta:
+#         __module__ = Blog
 
-    class Meta:
-        __module__ = Blog
 
+admin.site.register(Blog)
+admin.site.register(Tag)
+admin.site.register(Category)
 
-admin.site.register(Blog, BlogAdmin)
+# todo create a beautiful admin page
--- Blog/models.py
+++ Blog/models.py
@@ -1,13 +1,45 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    '''
+    標籤，多對多分類，多選
+    '''
+    title = models.CharField('標籤', max_length=30, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
+class Category(models.Model):
+    '''
+    分類，一對多，單選
+    '''
+    title = models.CharField('分類', max_length=30, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
 class Blog(models.Model):
     title = models.CharField("標題", max_length=150, unique=True, null=False, blank=False)
     createTime = models.DateTimeField("創建時間", auto_now_add=True, auto_now=False)
     updateTime = models.DateTimeField("修改時間", auto_now_add=False, auto_now=True)
     body = models.TextField("內容")
+    access_count = models.IntegerField("瀏覽量", default=1, editable=False)
+    category = models.ForeignKey('Category', verbose_name="分類集合")
     shortUrl = models.CharField("短URL", max_length=150, unique=True, null=False, blank=False)
-    # todo tags have not set
+    tags = models.ManyToManyField('Tag', verbose_name="標籤集合", null=True)
+    tags.help_text = '標籤集合'
+    # todo use uuid to create shortUrl
+    # todo create tags by textRank
 
     def __str__(self):
         return self.title
+
