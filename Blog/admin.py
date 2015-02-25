80a317a290b9a62df80f8f6b3418569349a702e2
hourong <nmghr9@gmail.com>
2015-02-25 15:33:31 +0800

    Change access_count name to accessCount Make an appearance of admin input page Change shortUrl editable=False Make an appearance of admin list page

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,15 +1,47 @@
 from django.contrib import admin
 from .models import *
+import uuid
 
+
+def getShortUrl():
+    shortUrl = str(uuid.uuid4()).replace('-', '')[:10]
+    try:
+        Blog.objects.get(shortUrl=shortUrl)
+        getShortUrl()
+    except:
+        return shortUrl
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'updateTime', 'createTime', 'shortUrl', 'accessCount')
+    search_fields = ('title', 'body')
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'category', ), 'body',),
+        }),
+        ('Advanced options', {
+            'classes': ('Blog', ),
+            'fields': ('tags', ),
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.shortUrl = getShortUrl()
+        obj.save()
+
+# todo create tags by textRank
+# todo jieba can't use in python3
+# todo remove code in fields to make it can use textRank
+# todo use openCC to change language in blog
 
 # class BlogAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'createTime', 'updateTime']
+# list_display = ['title', 'createTime', 'updateTime']
 #
-#     class Meta:
-#         __module__ = Blog
+# class Meta:
+# __module__ = Blog
 
 
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
 admin.site.register(Tag)
 admin.site.register(Category)
 
--- Blog/models.py
+++ Blog/models.py
@@ -32,14 +32,13 @@ class Blog(models.Model):
     createTime = models.DateTimeField("創建時間", auto_now_add=True, auto_now=False)
     updateTime = models.DateTimeField("修改時間", auto_now_add=False, auto_now=True)
     body = models.TextField("內容")
-    access_count = models.IntegerField("瀏覽量", default=1, editable=False)
+    accessCount = models.IntegerField("瀏覽量", default=1, editable=False)
     category = models.ForeignKey('Category', verbose_name="分類集合")
-    shortUrl = models.CharField("短URL", max_length=150, unique=True, null=False, blank=False)
+    shortUrl = models.CharField("短URL", max_length=150, unique=True, null=False, blank=False, editable=False)
     tags = models.ManyToManyField('Tag', verbose_name="標籤集合", null=True)
     tags.help_text = '標籤集合'
-    # todo use uuid to create shortUrl
-    # todo create tags by textRank
 
     def __str__(self):
         return self.title
+
 
