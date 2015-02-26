0e07c9a699294ce794f376d5db0f24ebdb623856
hourong <nmghr9@gmail.com>
2015-02-27 04:49:31 +0800

    PreCreate the view page Create the static base page Use highlight.js to make code highlighting

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
 
-def test(request):
-    return render(request, 'test.html')
+def home(request):
+    pass
+
+
+def rank(request):
+    pass
+
+
+def about(request):
+    pass
+
+
+def archive(request):
+    pass
+
+
+def search(request):
+    pass
+
+
+# todo create python function to complate the page automaticly
+# todo create <pre><code></code></pre> label to let highlight.js work
