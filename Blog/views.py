a103e1afdd17c24f8bceae90ae7e4e9eff972363
hourong <nmghr9@gmail.com>
2015-02-28 00:59:31 +0800

    Create home page Change base style Create archive page Make accessCount able Write urls page Make some a label can be use

--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import (render, get_object_or_404, get_list_or_404)
+from Blog.models import *
 
 
 def home(request):
-    pass
+    posts = Blog.objects.order_by('-updateTime')
+    categorys = Category.objects.all()
+    hotPassages = Blog.objects.order_by('-accessCount')[:10]
+    timeMachine = Blog.objects
+    return render(request, 'home.html', {'posts': posts, 'categorys': categorys, 'hotPassages': hotPassages})
 
 
 def rank(request):
@@ -13,8 +18,13 @@ def about(request):
     pass
 
 
-def archive(request):
-    pass
+def archive(request, shortUrl):
+    post = get_object_or_404(Blog, shortUrl=shortUrl)
+    post.accessCount += 1
+    post.save()
+    categorys = Category.objects.all()
+    hotPassages = Blog.objects.order_by('-accessCount')[:10]
+    return render(request, 'archive.html', {'post': post, 'categorys': categorys, 'hotPassages': hotPassages})
 
 
 def search(request):
--- Site/urls.py
+++ Site/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^test/', 'Blog.views.test')
+                       url(r'^archive/(?P<shortUrl>\w+)/', 'Blog.views.archive', name='archive'),
+                       url(r'^$', 'Blog.views.home', name='home'),
 )
